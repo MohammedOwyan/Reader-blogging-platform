23070a519037e93287d30c830f1354c1039b373b
mohamed owyan <mohamedislam2410@gmail.com>
2025-11-01 00:34:00 +0200

    remove unnecessary route

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -12,57 +12,83 @@ interface ArticlePageProps {
   params: { id: string };
 }
 
-export default async function ArticlePage({ params }: ArticlePageProps) {
+export async function generateMetadata({
+  params
+}: ArticlePageProps) {
+  const { id } = params;
+  const article = await GetArticle(Number(id));
+  return {
+    title: article?.title,
+    description: article?.summary,
+  };
+}
 
-  const { id } = await params
+export default async function ArticlePage({ params }: ArticlePageProps) {
+  const { id } = await params;
 
   // Fetch article from database
   const article = await GetArticle(Number(id));
 
-  console.log("article is :",article)
+  console.log("article is :", article);
 
   if (!article) return notFound();
   // if (!article.thumbnail){
   //   return "someting went wrong"
   // }
 
-  const safeHTML = sanitizeHTML(article.content)
+  const safeHTML = sanitizeHTML(article.content);
 
   return (
     <div className="flex flex-col">
       <NavigationBar />
       <main className=" w-1/2 self-center  py-24">
         <h1 className="text-6xl">{article.title}</h1>
-        <div className='flex justify-between items-center mt-16'>
+        <div className="flex justify-between items-center mt-16">
           <div className="flex items-center ">
-            <Avatar className='w-16 h-16 mr-4'>
-              <AvatarImage src={article.author.ProfilePictureUrl || '#'} />
+            <Avatar className="w-16 h-16 mr-4">
+              <AvatarImage src={article.author.ProfilePictureUrl || "#"} />
               <AvatarFallback>
                 {article.author.firstName.charAt(0).toUpperCase()}
                 {article.author.lastName.charAt(0).toUpperCase()}
               </AvatarFallback>
             </Avatar>
             <div className="flex flex-col">
-              <span className=''>{article.author.firstName + " " + article.author.lastName}</span>
+              <span className="">
+                {article.author.firstName + " " + article.author.lastName}
+              </span>
               <div>
-                <span className='text-sm text-gray-700 font-semibold'>{article.author.job || "UX Designer"}</span>
+                <span className="text-sm text-gray-700 font-semibold">
+                  {article.author.job || "UX Designer"}
+                </span>
                 {" . "}
-                <span className='text-sm text-gray-500'>{new Date(article.createdAt).toLocaleDateString("en-US", {
-                  month: "short",
-                  day: "2-digit",
-                  year: "numeric"
-                })}</span>
+                <span className="text-sm text-gray-500">
+                  {new Date(article.createdAt).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "2-digit",
+                    year: "numeric",
+                  })}
+                </span>
               </div>
             </div>
           </div>
-          <Button variant={"default"} className="py-0 text-xl">Follow</Button>
+          <Button variant={"default"} className="py-0 text-xl">
+            Follow
+          </Button>
         </div>
         <ActionButtons />
         <div>
-          <div className="w-full"><Image src={article.thumbnail||""} alt={"thumbnail"} className="w-full h-72" width={400} height={200}/></div>
-            <div dangerouslySetInnerHTML={{__html:safeHTML}}/>
+          <div className="w-full">
+            <Image
+              src={article.thumbnail || ""}
+              alt={"thumbnail"}
+              className="w-full h-72"
+              width={400}
+              height={200}
+            />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: safeHTML }} />
         </div>
-      </main >
+      </main>
     </div>
   );
 }
